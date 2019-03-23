4b29490396ee6f18d05335d0412ee82932b9d4d7
chau.bao.long <chau.bao.long@framgia.com>
2019-03-23 20:12:59 +0700

    Bootstrap project.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "gatsby";
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/">back to home</Link>
+    <h1>About Gatsby</h1>
+    <p>Such wow. Very React.</p>
+  </div>
+)
