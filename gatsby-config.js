179a1ec6dcdd79492f8ccca7c5eb169645f5ff7d
chau.bao.long <chau.bao.long@framgia.com>
2019-03-24 12:15:02 +0700

    Add ant-design theme.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-eslint',
     'gatsby-plugin-styled-components',
+    'gatsby-plugin-antd',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Button, Switch } from 'antd';
 
 const Container = styled.div`
   margin: 3rem auto;
@@ -10,60 +11,12 @@ const Container = styled.div`
   justify-content: center;
 `;
 
-const UserWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0 auto 12px auto;
-  &:last-child {
-    margin-bottom: 0;
-  }
-`;
-
-const Avatar = styled.img`
-  flex: 0 0 96px;
-  width: 96px;
-  height: 96px;
-  margin: 0;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  margin-left: 18px;
-  padding: 12px;
-`;
-
-const Username = styled.h2`
-  margin: 0 0 12px 0;
-  padding: 0;
-`;
-
-const Excerpt = styled.p`
-  margin: 0;
-`;
-
-const User = ({ avatar, username, excerpt }) => (
-  <UserWrapper>
-    <Avatar src={avatar} alt="" />
-    <Description>
-      <Username>{username}</Username>
-      <Excerpt>{excerpt}</Excerpt>
-    </Description>
-  </UserWrapper>
-);
-
 export default () => (
   <Container>
-    <h1>About Styled Components</h1>
-    <p>Styled Components is cool</p>
-    <User
-      username="Jane Doe"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
-    <User
-      username="Bob Smith"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-      excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    <Switch defaultChecked />
   </Container>
 );
