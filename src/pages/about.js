35d945c4dd7696c81feb8f78e8dafaeeda2fb8c9
chau.bao.long <chau.bao.long@framgia.com>
2019-03-24 20:40:39 +0700

    Check whether build cache work or not.

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,8 +14,6 @@ const Container = styled.div`
 export default () => (
   <Container>
     <Button type="primary">Primary</Button>
-    <Button>Default</Button>
-    <Button type="dashed">Dashed</Button>
     <Button type="danger">Danger</Button>
     <Switch defaultChecked />
   </Container>
