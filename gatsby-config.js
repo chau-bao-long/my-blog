4004f85d7a2294ad0c52d29c1c1effd30959b7d7
chau.bao.long <chau.bao.long@framgia.com>
2019-03-29 00:21:43 +0700

    Fix crazy bug relate to order of css in Gatsby.

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-eslint',
-    'gatsby-plugin-antd',
     'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
